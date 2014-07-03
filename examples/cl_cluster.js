4e6240e89d50fbb1dec98944489064b6cf3cd693
luxiaoyan <dabz0813@gmail.com>
2014-07-03 22:52:47 +0800

    luyan

--- examples/cl_cluster.js
+++ examples/cl_cluster.js
@@ -1,26 +1,30 @@
 var cluster = require('cluster');
-var cpsLimit = require('cpsLimit');
+var cpsLimit = require('../');
 var http = require('http');
 
 var conf = {
-    'app': 100,
+    'app': 2,
 }
 
 if (cluster.isMaster) {
     cpsLimit.master.init(conf);    
     cluster.on('online', function (worker) {
-        cpslimit.master.bind(worker); 
+        cpsLimit.master.bind(worker); 
     })
+    for (var i = 0; i < 2; i++) {
+        cluster.fork()
+    }
 } else {
     cpsLimit.worker.init();
     var server = http.createServer(function (request, response) {
         var isLimited = cpsLimit.worker.check('app');
         if (isLimited) {
             //do something... 
+            console.log('limited')
         }
     });
 
-    server.listen();
+    server.listen('8000');
 }
 
 
--- index.js
+++ index.js
@@ -4,64 +4,62 @@ var cpsLimit = module.exports = new EventEmitter;
 
 cpsLimit.init = function (conf) {
     if ('object' !== typeof conf) {
+        throw new Error('please be sure you have a correct conf');
         return false;
     }
-    this.cpsConf = conf;
+    this.conf = conf;
     this.isLimited = {};
     this.disabled = false;
     this.current = 0;
-    this.count = {};
+    this.counters = {};
 }
 
 cpsLimit.check = function (id) {
+    if ('undefined' === typeof this.conf[id]) {
+        return false;
+    }
+
     var seconds = Math.floor(new Date().getTime() / 1000);
     if (this.current !== seconds) {
         this.current = seconds;
-        this.count = {};
+        this.counters = {};
     } 
-    this.plus(id);
-    if (this.count[id] > (this.cpsConf[id] || 0)) {
+
+    this.counters[id] ? this.counters += 1 : this.counter = 1; 
+
+    if (this.counters[id] > this.cpsConf[id]) {
         return this.isLimited[id] = true;
     } else {
         return this.isLimited[id] = false;
     }
 }
 
-cpsLimit.plus = function (id) {
-    if (this.count[id]) {
-        this.count[id] = 1;
-    } else {
-        this.count[id] += 1;
-    } 
-};
-
-
 
 
 cpsLimit.master = new EventEmitter;
 
-cpsLimit.master.init = function (conf, cluster) {
-    if ('object' !== typeof cluster || 'object' !== typeof conf) {
+cpsLimit.master.init = function (conf) {
+    if ('object' !== typeof conf) {
+        throw new Error('please be sure you have a correct conf');
         return false;
     }
 
-    this.cluster = cluster;
     this.cpsConf = conf;
     this.isLimited = {};
     this.disabled = false;
     this.current = 0;
-    this.count = {};
+    this.counters = {};
     this.workers = [];
     
 };
 
 cpsLimit.master.bind = function (worker) {
-    var self = this;
-
-    if ('object' !== typeof cluster) {
+    if ('object' !== typeof worker) {
+        throw new Error('there is no worker process');
         return false;
     }
 
+    var self = this;
     this.workers.push(worker); 
 
     worker.on('message', function(msg) {
@@ -69,42 +67,36 @@ cpsLimit.master.bind = function (worker) {
             return false;
         }
         if (msg.action === 'cpsPlus') {
-            self.counter(msg.id);
+            self.count(msg.id);
         }
-        self.emit('plus', msg);
     })
 };
 
-cpsLimit.master.counter = function (id) {
+cpsLimit.master.count = function (id) {
     var seconds = Math.floor(new Date().getTime() / 1000);
     if (seconds !== this.current) {  //new second, clear all counts;
         this.current = seconds;
-        this.count = {};
+        this.counters = {};
         if (this.isLimited[id]) {
             this.isLimited[id] = false;
-            this.unlimitWorkers();
+            this.informWorkers({action: 'cpslimit', id: id, isLimited: false});
         }
     }
-    if (!this.count[id]) {
-        this.count[id] = 0;
+    if (!this.counters[id]) {
+        this.counters[id] = 0;
     }
-    this.count[id] += 1;
-    if (this.count[id] > (this.cpsConf[id] || 0)) {
+    this.counters[id] += 1;
+    if (this.counters[id] > (this.cpsConf[id] || 0)) {
         this.isLimited[id] = true;
-        this.limitWorkers(); 
+        this.informWorkers({action: 'cpslimit', id: id, isLimited: true}); 
     }
 
 };
 
-cpsLimit.master.limitWorkers = function () {
-    for (var worker_id in self.workers) {
-        self.workers[worker_id].send({action: 'cpsLimit', id: id, isLimited : true});
-    }
-}
-
-cpsLimit.master.unlimitWorkers = function () {
-    for (var worker_id in self.workers) {
-        self.workers[worker_id].send({action: 'cpsLimit', id: id, isLimited : false});
+cpsLimit.master.informWorkers = function (msg, workers) {
+    workers = workers || this.workers;
+    for (var i in workers) {
+        workers[i].send(msg);
     }
 }
 
@@ -122,12 +114,9 @@ cpsLimit.master.disable = function () {
 cpsLimit.worker = new EventEmitter;
 
 cpsLimit.worker.init = function () {
-    this.isLimited = {};
-    this.bind();
-};
-
-cpsLimit.worker.bind = function () {
     var self = this;
+    this.isLimited = {};
+
     process.on('message', function (msg) {
         if ('object' === typeof msg && msg.action === 'cpsLimit') {
             self.isLimited[msg.id] = msg.isLimited;
@@ -147,4 +136,5 @@ cpsLimit.worker.check = function (id) {
 cpsLimit.worker.plus = function (id) {
     process.send({action: 'cpsPlus', id: id,});
 }
+
 
